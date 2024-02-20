f7505f3c618a0200f8f9edf65e9a3d292930f457
Rakhaa2 <raditsyamsul6@gmail.com>
2024-02-20 11:26:38 +0700

    tugas1

---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,6 @@
+<?php
+$topi = "Topi";
+$bundar = "Bundar";
+
+echo "<h2><em>" . " ”" . $topi . " Saya Bundar, " . $bundar . " Topi saya.”" . "</em></h2>";
+?>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,12 @@
+<?php
+$npm_syamsul = 32;
+echo "Aku adalah angka <strong> $npm_syamsul </strong> <br>";
+$npm_syamsul *= 5;
+echo "Jika aku dikali 5,maka aku sekarang <strong>$npm_syamsul</strong> <br>";
+$npm_syamsul /= 2;
+echo "Jika aku dibagi 2,maka aku sekarang <strong>$npm_syamsul</strong> <br>";
+$npm_syamsul += 75;
+echo "Jika aku ditambah 75,maka aku sekarang <strong>$npm_syamsul</strong> <br>";
+$npm_syamsul -= 20;
+echo "Jika aku dikurang 20,maka aku sekarang <strong>$npm_syamsul</strong> <br>";
+?>
